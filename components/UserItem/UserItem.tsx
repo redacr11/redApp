65bd50b5e24a3767d5a076f6d8b565ae3833e1b1
Reda <reda.cr@outlook.com>
2021-11-11 08:21:57 +0400

    users& new chatrooms

--- components/UserItem/UserItem.tsx
+++ components/UserItem/UserItem.tsx
@@ -2,12 +2,37 @@ import React from "react";
 import { Text, View, Image, Pressable } from "react-native";
 import { useNavigation } from "@react-navigation/core";
 import styles from "./styles";
+import { DataStore, Auth } from "aws-amplify";
+import { ChatRoom, User, ChatRoomUser } from "../../src/models";
 
 export default function UserItem({ user }) {
   const navigation = useNavigation();
 
-  const onPress = () => {
+  const onPress = async () => {
+    //TODO if there is already a chatroom between these 2 users then redirect to the existing chatroom, otherwise, create a new chatroom
+
     // Create a chatroom with the person
+    const newChatRoom = await DataStore.save(new ChatRoom({ newMessages: 0 }));
+
+    // Connect authenticated user with the chat room
+    const authUser = await Auth.currentAuthenticatedUser();
+    const dbUser = await DataStore.query(User, authUser.attributes.sub);
+    await DataStore.save(
+      new ChatRoomUser({
+        user: dbUser,
+        chatroom: newChatRoom,
+      })
+    );
+
+    // Connect clicked user with the chatroom
+    await DataStore.save(
+      new ChatRoomUser({
+        user,
+        chatroom: newChatRoom,
+      })
+    );
+
+    navigation.navigate("ChatRoom", { id: newChatRoom.id });
   };
   return (
     <Pressable onPress={onPress} style={styles.container}>
--- screens/UsersScreen.tsx
+++ screens/UsersScreen.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View, FlatList } from "react-native";
-import Users from "../assets/dummy-data/Users";
+import { DataStore } from "@aws-amplify/datastore";
+
 import UserItem from "../components/UserItem";
+import { User } from "../src/models";
 
 export default function UsersScreen() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    DataStore.query(User).then(setUsers);
+  });
+
+  //! ^^ is a better way since there is not a big chunk of code
+  // useEffect(() => {
+  //   // query users
+  //   const fetchUsers = async () => {
+  //     const fetchedUsers = await DataStore.query(User);
+  //     setUsers(fetchedUsers);
+  //   };
+
+  //   fetchUsers();
+  // }, []);
+
   return (
     <View style={styles.page}>
       <FlatList
-        data={Users}
+        data={users}
         renderItem={({ item }) => <UserItem user={item} />}
         showsVerticalScrollIndicator={false}
       />
